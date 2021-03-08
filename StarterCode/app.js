6528133aacf70b187c9c6e14c0a21b6dce970e80
Simon Feinsilver <sdfeinsilver@gmail.com>
2021-03-08 17:43:46 -0600

    Successfully logged data to conole

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -2,6 +2,17 @@
 d3.json("data/samples.json").then((data) => {
     // Create the Traces
     let trace1 = {
-        x: data.
+        x: data.sample_values,
+        y: data.otu_ids,
+        type: "box"
     }
-}
+    // Create the data array for the plot
+    let traceDataArr = [trace1];
+    // Define the plot layout
+    let layout = {
+        title: "Top 10 OTU's"
+    }
+
+    // Plot the chart to a div tag with id "selDataset"
+    Plotly.newPlot("selDataset", traceDataArr, layout);
+});
