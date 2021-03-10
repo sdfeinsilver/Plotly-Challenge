0e7cc6a93f6037e81dadece67a5bc3fdd1e1a414
Simon Feinsilver <sdfeinsilver@gmail.com>
2021-03-10 11:33:55 -0600

    Successfully bound subjectId data to dropdown menu

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -6,85 +6,16 @@ let userSubjectChoice = d3.select("#selDataset");
 
 // 2. Read in json data and start
 d3.json("data/samples.json").then((data) => {
-    // Check data in console
+    // Check data can log in console -- CHECK!
     console.log(data);
+    // Assign local variable to subject ids, and check that you can log to console -- CHECK!
+    let subjectIds = data.names;
+    console.log(subjectIds);
+    // Append subjectIds to userSubjectChoice handle
+    subjectIds.forEach((id) => {
+        userSubjectChoice
+            .append("option")
+            .property("value", id)
+            .text(id);
 });
-
-
-
-
-
-
-
-
-
-
-
-// // Put the names in the samples.json file in the dropdown box
-
-// var select_tag = d3.select("#selDataset");
-
-// d3.json("data/samples.json").then((data) => {
-//     var subject_ids = importedData.names;
-  
-
-
-// // Check to see that you can log the samples.json names to the console
-// d3.json("data/samples.json").then((data) => {
-//     console.log(data.names);
-
-//     subject_ids.forEach((id) => {
-//         select_tag
-//           .append("option")
-//           .property("value", id)
-//           .text(id);
-//       });
-    
-// });
-
-
-
-
-
-
-
-
-
-// // Log sample_values to console to test
-// d3.json("data/samples.json").then((data) => {
-//     console.log(data.sample_values);
-// });
-
-
-// // // Define Dataset you are working with:
-// // // 1. Define individual dataset
-// // let individual = 
-
-
-// // //Function to draw a horizontal bar chart based on whichever option selected int he dropdown
-// // function drawHorBar (dataset) {
-
-// // }
-
-
-
-
-
-// // // Read in the json file
-// // d3.json("data/samples.json").then((data) => {
-// //     // Create the Traces
-// //     let trace1 = {
-// //         x: data.sample_values,
-// //         y: data.otu_ids,
-// //         type: "box"
-// //     }
-// //     // Create the data array for the plot
-// //     let traceDataArr = [trace1];
-// //     // Define the plot layout
-// //     let layout = {
-// //         title: "Top 10 OTU's"
-// //     }
-
-// //     // Plot the chart to a div tag with id "selDataset"
-// //     Plotly.newPlot("selDataset", traceDataArr, layout);
-// // });
+});
